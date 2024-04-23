bebe901905aed5afbe8ac11ef242e3fe52de19f4
butalsoverycool <37105703+butalsoverycool@users.noreply.github.com>
2024-04-23 22:15:38 +0200

    fix css vs tw class conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.liquid"],
+  blocklist: [
+    'grid', /* conflicting with grid class in base.css */
+  ],
   theme: {
     extend: {},
   },
